f954c8ecce81014bf07a242b8e89ec709cd95969
Brendan Koning <brendankoning@brendans-mac-mini.local>
2015-10-10 21:39:36 -0400

    Initial Commit

---
+++ SimpleContactsApplicationTests/SimpleContactsApplicationTests.swift
@@ -0,0 +1,36 @@
+//
+//  SimpleContactsApplicationTests.swift
+//  SimpleContactsApplicationTests
+//
+//  Created by Brendan Koning on 10/10/15.
+//  Copyright © 2015 Brendan Koning. All rights reserved.
+//
+
+import XCTest
+@testable import SimpleContactsApplication
+
+class SimpleContactsApplicationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
